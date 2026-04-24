4930139c91a1835d8c15acfb0d9a75ae189fca76
ivogiake <259051186+JohnStarlight@users.noreply.github.com>
2026-04-24 20:56:02 +0300

    created a functional main.go with user choice of ascii style

--- cmd/main.go
+++ cmd/main.go
@@ -1,4 +1,38 @@
 package main
 
+import (
+	"fmt"
+	"os"
+
+	"ascii-art/internal"
+)
+
 func main() {
+	if len(os.Args) != 2 {
+		fmt.Println("The correct usage of this program is:")
+		fmt.Println("go run . \"your-text-here\"")
+		return
+	}
+	text := os.Args[1]
+	fmt.Println("In which style would you like that?")
+	fmt.Println("1 = Standard")
+	fmt.Println("2 = Shadow")
+	fmt.Println("3 = Thinkertoy")
+
+	var choice int
+	var filename string
+	fmt.Scan(&choice)
+
+	switch choice {
+	case 1:
+		filename = "standard.txt"
+	case 2:
+		filename = "shadow.txt"
+	case 3:
+		filename = "thinkertoy.txt"
+	default:
+		fmt.Println("The acceptable input is 1, 2, 3.")
+		return
+	}
+	fmt.Printf("%s", internal.PrintAscii(text, filename))
 }
